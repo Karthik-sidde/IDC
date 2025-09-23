a6344fd19ef229ed060fe1657f753f47788c2ebb
Karthik Sidde <karthisidde@gmail.com>
2025-09-23 12:57:50 +0000

    different color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center p-4 flex-col bg-gradient-to-br from-background to-primary/10">
+    <main className="flex min-h-screen w-full items-center justify-center p-4 flex-col bg-gradient-to-tr from-secondary via-background to-secondary/20">
       <div className="absolute top-4 left-4 z-10">
           <AppLogo />
       </div>
